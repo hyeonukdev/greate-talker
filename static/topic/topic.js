8d195fe40fd14446decff602fa1653af87ca17fe
hyeonukdev <lego7407@gmail.com>
2021-04-26 23:41:04 +0900

    update topic

--- static/topic/topic.js
+++ static/topic/topic.js
@@ -100,5 +100,43 @@ $(document).ready(function() {
         
     });
     //*********** 1depth카테고리 선택 후 2depth 생성 END ***********
-        
+
+});
+
+// 테이블의 Row 클릭시 값 가져오기
+$("#topic-select tr").click(function(){
+
+    var str = ""
+    var tdArr = new Array();	// 배열 선언
+
+    // 현재 클릭된 Row(<tr>)
+    var tr = $(this);
+    var td = tr.children();
+
+    // tr.text()는 클릭된 Row 즉 tr에 있는 모든 값을 가져온다.
+    console.log(tr.text());
+    //
+    // // 반복문을 이용해서 배열에 값을 담아 사용할 수 도 있다.
+    // td.each(function(i){
+    //     tdArr.push(td.eq(i).text());
+    // });
+    //
+    // console.log("배열에 담긴 값 : "+tdArr);
+    //
+    // // td.eq(index)를 통해 값을 가져올 수도 있다.
+    // var no = td.eq(0).text();
+    // var userid = td.eq(1).text();
+    // var name = td.eq(2).text();
+    // var email = td.eq(3).text();
+    //
+    //
+    // str +=	" * 클릭된 Row의 td값 = No. : <font color='red'>" + no + "</font>" +
+    //         ", 아이디 : <font color='red'>" + userid + "</font>" +
+    //         ", 이름 : <font color='red'>" + name + "</font>" +
+    //         ", 이메일 : <font color='red'>" + email + "</font>";
+    // //
+    if (tr.text() == "친구")
+        $("#topic-show").text("친구");
+   if (tr.text() == "사랑")
+        $("#topic-show").html("사랑");
 });
--- topic/views.py
+++ topic/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+from .models import Choice
 
 def show_topic(request):
-    return render(request, 'show_topic.html')
+    topics = Question.objects
+    choices = Choice.objects
+    choices_friends = Choice.objects.filter(question_id=1)
+    choices_love = Choice.objects.filter(question_id=3)
+    print("topics: {}".format(topics))
+    print("choice: {}".format(choices))
+    return render(request, 'show_topic.html', {'topics': topics, 'choices': choices, 'choices_friends':choices_friends, 'choices_love':choices_love})
